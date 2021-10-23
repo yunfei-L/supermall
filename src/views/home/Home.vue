3cb13b8868ca74a2505d5fd52b05bd19c769a0ed
Luba <2571407672@qq.com>
2021-10-23 17:02:37 +0800

    Swiper & Recommends

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,37 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from 'common/navbar/NavBar'
+import HomeSwiper from './childComps/HomeSwiper.vue'
+import RecommendView from './childComps/Recommend.vue'
+
+import {getHomeMultidata} from 'network/home'
+
 export default {
     name: "Home",
     components:{
-      NavBar
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    data(){
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created(){
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+        console.log(res)
+      })
     }
 }
 </script>
