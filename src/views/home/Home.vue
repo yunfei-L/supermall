77b9e203d7ebb42a4480b3e638998331b7013090
Luba <2571407672@qq.com>
2021-10-24 16:40:50 +0800

    home finished v1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,124 +1,148 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tab-control"
-                 :titles = "['流行','新款','精选']"
-                 @tabClick="tabClick"
-                 ></tab-control>
-    <good-list :goods="showGoods"></good-list>
+    <scroll class="content" ref="Scroll" :probe-type="3" :pull-up-load="true" @scroll="contentScroll" @pullingUp="loadMore">
+      <home-swiper :banners="banners"></home-swiper>
+        <recommend-view :recommends="recommends"></recommend-view>
+        <feature-view></feature-view>
+        <tab-control
+          class="tab-control"
+          :titles="['流行', '新款', '精选']"
+          @tabClick="tabClick"
+        ></tab-control>
+        <good-list :goods="showGoods"></good-list>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShowBackTop"></back-top>
   </div>
 </template>
 
 <script>
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/Recommend.vue";
+import FeatureView from "./childComps/FeatureView.vue";
 
-import HomeSwiper from './childComps/HomeSwiper.vue'
-import RecommendView from './childComps/Recommend.vue'
-import FeatureView from './childComps/FeatureView.vue'
+import NavBar from "common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodList";
+import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
-import NavBar from 'common/navbar/NavBar'
-import TabControl from 'components/content/tabControl/TabControl'
-import GoodList from 'components/content/goods/GoodList'
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 
-import {getHomeMultidata,
-        getHomeGoods
-        } from 'network/home'
 
 export default {
-    name: "Home",
-    components:{
-      HomeSwiper,
-      RecommendView,
-      FeatureView,
-      NavBar,
-      TabControl,
-      GoodList
-    },
-    data(){
-      return {
-        banners: [],
-        recommends: [],
-        goods: {
-          'pop':{page:0, list:[]},
-          'new':{page:0, list:[]},
-          'sell':{page:0, list:[]}
-        },
-        currentType:'pop'
-      }
-    },
-    computed:{
-      showGoods(){
-        return this.goods[this.currentType].list
-      }
-    },
-    created(){
-      this.getHomeMultidata()
-
-      this.getHomeGoods('pop')
-      this.getHomeGoods('new')
-      this.getHomeGoods('sell')
-     
-    },
-    methods:{
-      //事件监听相关的方法
-      tabClick(index){
-        switch(index){
-          case 0:
-            this.currentType = 'pop'
-            break
-          case 1:
-            this.currentType = 'new'
-            break
-          case 2:
-            this.currentType = 'sell'
-            break
-
-        }
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodList,
+    Scroll,
+    BackTop
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
       },
+      currentType: "pop",
+      isShowBackTop:false
+    };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    },
+  },
+  created() {
+    this.getHomeMultidata();
 
-      //网络请求相关的方法
-      getHomeMultidata(){
-        getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list
-        this.recommends = res.data.recommend.list
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    //事件监听相关的方法
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    backClick(){
+      this.$refs.Scroll.scrollTo(0,0,500)
+    },
+    contentScroll(position){
+      this.isShowBackTop = -position.y > 1000
+    },
+    loadMore(){
+      this.getHomeGoods(this.currentType)
+    },
+
+    //网络请求相关的方法
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
         // console.log(res)
-      })
-      },
-      getHomeGoods(type){
-        const page = this.goods[type].page + 1
-        getHomeGoods(type,page).then(res => {
-          this.goods[type].list.push(...res.data.list)
-          this.goods[type].page += 1
-          console.log(res)
-      })
-      }
-      
-       
-    }
-}
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+        this.$refs.Scroll.finishPullUp()
+      });
+    },
+    
+  },
+};
 </script>
 
-<style>
- .home-nav{
-   background-color:var(--color-tint);
-   color: white;
-   position: fixed;
-   left: 0;
-   right: 0;
-   top: 0;
-   z-index: 9;
- }
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
 
- #home{
-   padding-top: 44px;
- }
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
 
- .tab-control{
-   position: sticky;
-   top: 44px;
-   z-index: 9;
- }
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+
+.content{
+  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
 </style>
