d69ed243b5dd4a73fcfac26c5c7f491c00d714e4
Luba <2571407672@qq.com>
2021-11-02 16:43:32 +0800

    debounce

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -48,6 +48,7 @@ export default {
           this.scroll && this.scroll.finishPullUp()
       },
       refresh(){
+        console.log('-----')
           this.scroll && this.scroll.refresh()
       }
   }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -71,13 +71,21 @@ export default {
     
   },
   mounted(){
+    const refresh = this.debounce(this.$refs.Scroll.refresh,500)
     this.$bus.$on('itemImageLoad',()=>{
-      console.log('----')
-      this.$refs.Scroll.refresh()
+      refresh()
     })
   },
   methods: {
-    //事件监听相关的方法
+    debounce(func,delay){
+      let timer = null
+      return function(...args){
+        if(timer) clearTimeout(timer)
+        timer = setTimeout(()=>{
+          func.apply(this,args)
+        },delay)
+      }
+    },
     tabClick(index) {
       switch (index) {
         case 0:
