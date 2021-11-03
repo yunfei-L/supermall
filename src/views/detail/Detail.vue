35c2962b7c2902300110032e2e97d444cfa84c42
Luba <2571407672@qq.com>
2021-11-03 16:05:10 +0800

    detail-base-info

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,45 @@
 <template>
     <div id="detail">
         <detail-nav-bar></detail-nav-bar>
+        <detail-swiper :top-images="topImages"></detail-swiper>
+        <detail-base-info :goods="goods"></detail-base-info>
     </div>
 </template>
 
 <script>
 import DetailNavBar from './childCompos/DetailNavBar.vue'
+import DetailSwiper from './childCompos/DetailSwiper.vue'
+import DetailBaseInfo from './childCompos/DetailBaseInfo.vue'
+
+import {getDetail, Goods} from '../../network/detail'
+
+
 export default {
     name:'Detail',
     components:{
-        DetailNavBar
+        DetailNavBar,
+        DetailSwiper,
+        DetailBaseInfo
     },
     data(){
         return {
-            iid:null
+            iid:null,
+            topImages:[],
+            goods:{}
         }
     },
     created(){
         // console.log(this.$route.params)
         this.iid = this.$route.params.id
+
+        getDetail(this.iid).then(res => {
+            const data = res.result
+            //1.获取顶部的图片轮播图数据
+            this.topImages = data.itemInfo.topImages
+
+            //2.获取商品信息
+            this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
+        })
     }
 }
 </script>
