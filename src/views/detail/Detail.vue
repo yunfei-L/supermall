5bcbc10e07ec8b3354d9957d45495cb0a598ca74
Luba <2571407672@qq.com>
2021-11-04 17:21:33 +0800

    detail linkage effect

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="detail">
-    <detail-nav-bar class="detail-nav"></detail-nav-bar>
-    <scroll class="content" :probe-type="3" :pull-up-load="true" ref="scroll">
+    <detail-nav-bar
+      class="detail-nav"
+      @titleClick="titleClick"
+      ref="nav"
+    ></detail-nav-bar>
+    <scroll class="content" :probe-type="3" :pull-up-load="true" ref="scroll" @scroll="contentScroll">
       <detail-swiper :top-images="topImages"></detail-swiper>
       <detail-base-info :goods="goods"></detail-base-info>
       <detail-shop-info :shop="shop"></detail-shop-info>
-      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
-      <detail-param-info :param-info="paramInfo"></detail-param-info>
-      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
-      <good-list :goods= "recommends"></good-list>
+      <detail-goods-info
+        :detail-info="detailInfo"
+        @imageLoad="imageLoad"
+      ></detail-goods-info>
+      <detail-param-info
+        ref="params"
+        :param-info="paramInfo"
+      ></detail-param-info>
+      <detail-comment-info
+        ref="comment"
+        :comment-info="commentInfo"
+      ></detail-comment-info>
+      <good-list ref="recommend" :goods="recommends"></good-list>
     </scroll>
   </div>
 </template>
@@ -19,14 +32,19 @@ import DetailSwiper from "./childCompos/DetailSwiper.vue";
 import DetailBaseInfo from "./childCompos/DetailBaseInfo.vue";
 import DetailShopInfo from "./childCompos/DetailShopInfo.vue";
 import DetailParamInfo from "./childCompos/DetailParamInfo.vue";
-import DetailCommentInfo from "./childCompos/DetailCommentInfo.vue"
-
+import DetailCommentInfo from "./childCompos/DetailCommentInfo.vue";
 
 import Scroll from "components/common/scroll/Scroll";
-import { getDetail, Goods, Shop,GoodsParam,getRecommend} from "../../network/detail";
-import DetailGoodsInfo from './childCompos/DetailGoodsInfo.vue';
-import GoodList from 'components/content/goods/GoodList'
-import {itemListenerMixin} from '../../common/mixin'
+import {
+  getDetail,
+  Goods,
+  Shop,
+  GoodsParam,
+  getRecommend,
+} from "../../network/detail";
+import DetailGoodsInfo from "./childCompos/DetailGoodsInfo.vue";
+import GoodList from "components/content/goods/GoodList";
+import { itemListenerMixin } from "../../common/mixin";
 
 export default {
   name: "Detail",
@@ -41,17 +59,19 @@ export default {
     DetailCommentInfo,
     GoodList,
   },
-  mixins:[itemListenerMixin],
+  mixins: [itemListenerMixin],
   data() {
-    return{
+    return {
       iid: null,
       topImages: [],
       goods: {},
       shop: {},
-      detailInfo:{},
-      paramInfo:{},
-      commentInfo:{},
-      recommends:[]
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
+      recommends: [],
+      themeTopYs: [],
+      currentIndex:0
     };
   },
   created() {
@@ -59,9 +79,9 @@ export default {
     this.iid = this.$route.params.id;
     //请求详情数据
     getDetail(this.iid).then((res) => {
-      const data = res.result
+      const data = res.result;
       //1.获取顶部的图片轮播图数据
-      this.topImages = data.itemInfo.topImages
+      this.topImages = data.itemInfo.topImages;
 
       //2.获取商品信息
       this.goods = new Goods(
@@ -71,35 +91,68 @@ export default {
       );
 
       //3.创建店铺信息的对象
-      this.shop = new Shop(data.shopInfo)
+      this.shop = new Shop(data.shopInfo);
 
       //4.保存商品的详情数据
-      this.detailInfo = data.detailInfo
+      this.detailInfo = data.detailInfo;
 
       //5.获取参数的信息
-      this.paramInfo = new GoodsParam(data.itemParams.info,data.itemParams.rule)
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
 
       //6.取出评论信息
-      if(data.rate.cRate !== 0){
-        this.commentInfo = data.rate.list[0]
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0];
       }
+
+      this.$nextTick(() => {
+        //根据最新的数据，对应的DOM是已经被渲染出来
+        //但是图片依然是没有加载出来
+        //offsetTop值不对的时候，都是因为图片的问题
+        
+      });
     });
     //获取推荐数据
-    getRecommend().then(res => {
-      this.recommends = res.data.list
-    })
+    getRecommend().then((res) => {
+      this.recommends = res.data.list;
+    });
   },
-  methods:{
-      imageLoad(){
-          this.$refs.scroll.refresh()
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+      this.themeTopYs.push(0);
+        this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+        this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+        this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+        this.themeTopYs.push(Number.MAX_VALUE)
+        // console.log(this.themeTopYs)
+    },
+    titleClick(index) {
+      this.$refs.scroll.scrollTo(0, -this.themeTopYs[index], 100);
+    },
+    contentScroll(position){
+      // console.log(position)
+      //1.获取y值
+      const positionY = -position.y
+      // console.log(positionY)
+
+      //2.positionY和主题中值进行比较
+      let length = this.themeTopYs.length
+      for(let i=0; i< length-1; i++){
+        if(this.currentIndex !== i &&(positionY >= this.themeTopYs[i] && positionY < this.themeTopYs[i+1])){
+            this.currentIndex = i
+            this.$refs.nav.$el.currentIndex = this.currentIndex
+        }
       }
+
+    }
   },
-  mounted(){
-    // console.log(2)
+  mounted() {},
+  destroyed() {
+    this.$bus.$off("itemImgLoad", this.itemImgListener);
   },
-  destroyed(){
-    this.$bus.$off('itemImgLoad',this.itemImgListener)
-  }
 };
 </script>
 
@@ -119,21 +172,21 @@ export default {
     height: calc(100% -44px);
 } */
 
- #detail {
-    height: 100vh;
-    position: relative;
-    z-index: 1;
-    background-color: #fff;
-  }
+#detail {
+  height: 100vh;
+  position: relative;
+  z-index: 1;
+  background-color: #fff;
+}
 
-  .content {
-    position: absolute;
-    top: 44px;
-    bottom: 60px;
-  }
-  .detail-nav{
-      position: relative;
-      z-index: 9;
-      background-color: #fff;
-  }
+.content {
+  position: absolute;
+  top: 44px;
+  bottom: 60px;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
